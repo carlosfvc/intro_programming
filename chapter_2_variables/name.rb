a75dc239e676ed50431f22faf439b48675fbf739
Carlos Costa <cfvieiracosta@gmail.com>
2018-11-27 18:54:27 +0000

    Updates chapter 2 and adding chapter 3

--- chapter_2_variables/name.rb
+++ chapter_2_variables/name.rb
@@ -1,4 +1,14 @@
-puts "What's your name?"
-name = gets.chomp
+puts "What's your first name?"
+first_name = gets.chomp
 
-puts "Hello #{name}, How are you?"
+puts "What's your second name?"
+second_name = gets.chomp
+
+puts "Hello #{first_name} #{second_name}, How are you?"
+
+#Prints names X times
+puts "How many reps?"
+x = gets.chomp.to_i
+x.times do
+    puts first_name + " " + second_name
+end
